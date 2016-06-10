4a9f79531d2678369f7cf39e12fd518d8ac6830a
gapillan <gpaillan@my.centennialcollege.ca>
2016-06-10 13:40:20 -0700

    Update by using PersonClassDiagram

--- PersonProject/InternationalStudent.cs
+++ PersonProject/InternationalStudent.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace COMP123_S2016_Lesson5
+namespace PersonProject
 {
+    public class InternationalStudent : Student
+    {
+    }
 }
---
+++ PersonProject/Teacher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersonProject
+{
+    public class Teacher : Person
+    {
+        private string _employeeNumber;
+
+        public Teacher(int age, string name, string employeeNumber)
+            : base(age, name) 
+        {
+        }
+
+        public string EmployeeNumber
+        {
+            get
+            {
+                return this._employeeNumber;
+            }
+            set
+            {
+                this._employeeNumber = value;
+            }
+        }
+
+        public void Teaches()
+        {
+            Console.WriteLine("{0} is Teaching!", this.Name);
+        }
+    }
+}
